2959bb81f512f6e24fa7ddfbf3162610e550c7ab
Oscar Franco <ospfranco@gmail.com>
2023-11-07 06:49:47 +0100

    Add logs while records are being created

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -29,74 +29,81 @@ export default function App() {
   };
 
   const queryLargeDb = async () => {
-    setIsLoading(true);
-    const times = await queryLargeDB();
-    setTimes(times.loadFromDb);
-    setAccessingTimes(times.access);
-    setIsLoading(false);
+    try {
+      setIsLoading(true);
+      const times = await queryLargeDB();
+      setTimes(times.loadFromDb);
+      setAccessingTimes(times.access);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <SafeAreaView className="flex-1 bg-neutral-900">
-      <ScrollView className="p-4">
-        <Text className=" text-white text-xl">OP SQLite</Text>
+      <View className="p-4">
+        <ScrollView>
+          <Text className=" text-white text-xl">OP SQLite</Text>
 
-        <Text className="text-lg text-white mt-8">Benchmarks</Text>
-        <Button title="Create 300k Record DB" onPress={createLargeDb} />
-        <Button title="Query 300k Records" onPress={queryLargeDb} />
-        {isLoading && <ActivityIndicator color={'white'} size="large" />}
+          <Text className="text-lg text-white mt-8">Benchmarks</Text>
+          <Button title="Create 300k Record DB" onPress={createLargeDb} />
+          <Button title="Query 300k Records" onPress={queryLargeDb} />
+          {isLoading && <ActivityIndicator color={'white'} size="large" />}
 
-        {!!times.length && (
-          <Text className="text-lg text-green-500 self-center">
-            Load from db{' '}
-            {(times.reduce((acc, t) => (acc += t), 0) / times.length).toFixed(
-              0,
-            )}{' '}
-            ms average
-          </Text>
-        )}
-        {!!accessingTimes.length && (
-          <Text className="text-lg text-green-500 self-center">
-            Read property{' '}
-            {(
-              accessingTimes.reduce((acc, t) => (acc += t), 0) /
-              accessingTimes.length
-            ).toFixed(0)}{' '}
-            ms average
-          </Text>
-        )}
+          {!!times.length && (
+            <Text className="text-lg text-green-500 self-center">
+              Load from db{' '}
+              {(times.reduce((acc, t) => (acc += t), 0) / times.length).toFixed(
+                0,
+              )}{' '}
+              ms average
+            </Text>
+          )}
+          {!!accessingTimes.length && (
+            <Text className="text-lg text-green-500 self-center">
+              Read property{' '}
+              {(
+                accessingTimes.reduce((acc, t) => (acc += t), 0) /
+                accessingTimes.length
+              ).toFixed(0)}{' '}
+              ms average
+            </Text>
+          )}
 
-        <Text className="text-lg text-white mt-8">Tests</Text>
-        {results
-          .sort((a: any, b: any) => {
-            return a.type === 'incorrect' ? -1 : 1;
-          })
-          .map((r: any, i: number) => {
-            if (r.type === 'grouping') {
-              return null;
-            }
+          <Text className="text-lg text-white mt-8">Tests</Text>
+          {results
+            .sort((a: any, b: any) => {
+              return a.type === 'incorrect' ? -1 : 1;
+            })
+            .map((r: any, i: number) => {
+              if (r.type === 'grouping') {
+                return null;
+              }
 
-            if (r.type === 'incorrect') {
+              if (r.type === 'incorrect') {
+                return (
+                  <View className="border-b border-neutral-600 py-2 flex-row">
+                    <Text key={i} className="text-white flex-1">
+                      {r.description}: {r.errorMsg}
+                    </Text>
+                    <Text>🟥</Text>
+                  </View>
+                );
+              }
+
               return (
                 <View className="border-b border-neutral-600 py-2 flex-row">
                   <Text key={i} className="text-white flex-1">
-                    {r.description}: {r.errorMsg}
+                    {r.description}
                   </Text>
-                  <Text>🟥</Text>
+                  <Text>🟩</Text>
                 </View>
               );
-            }
-
-            return (
-              <View className="border-b border-neutral-600 py-2 flex-row">
-                <Text key={i} className="text-white flex-1">
-                  {r.description}
-                </Text>
-                <Text>🟩</Text>
-              </View>
-            );
-          })}
-      </ScrollView>
+            })}
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
