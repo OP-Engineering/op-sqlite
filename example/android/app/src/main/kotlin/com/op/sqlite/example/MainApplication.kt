5a2780e98ef3a58831e1046ec9255414c00aab0d
Oscar Franco <ospfranco@gmail.com>
2023-12-08 08:29:14 +0100

    Enable new arch and remove safe-area-context

--- example/android/app/src/main/kotlin/com/op/sqlite/example/MainApplication.kt
+++ example/android/app/src/main/kotlin/com/op/sqlite/example/MainApplication.kt
@@ -39,6 +39,8 @@ class MainApplication : Application(), ReactApplication {
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
+      // Enable bridgeless mode
+      // load(bridgelessEnabled = true)
     }
     ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
