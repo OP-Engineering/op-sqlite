96d7bf451f9238abc74b8e52d3e9cea24053feb1
Oscar Franco <ospfranco@gmail.com>
2023-12-22 09:58:23 +0100

    Migrate to yarn v4

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'babel-plugin-transform-typescript-metadata',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-  ],
 };
