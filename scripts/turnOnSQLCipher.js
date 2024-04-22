5fa5bb116b1623e923c72f69d69a32cf9b372a4b
Oscar Franco <ospfranco@gmail.com>
2024-04-22 20:18:32 +0200

    Fix update script

--- scripts/turnOnSQLCipher.js
+++ scripts/turnOnSQLCipher.js
@@ -3,12 +3,14 @@ const fs = require('fs');
 console.log('Current working directory:', process.cwd());
 
 // Read the package.json file
-const packageJson = JSON.parse(fs.readFileSync('package.json'));
+const packageJson = JSON.parse(fs.readFileSync('./example/package.json'));
+
+console.log("ROPO package.json:", packageJson);
 
 // Modify the op-sqlite.sqlcipher key to true
 packageJson['op-sqlite']['sqlcipher'] = true;
 
 // Save the updated package.json file
-fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+fs.writeFileSync('./example/package.json', JSON.stringify(packageJson, null, 2));
 
 console.log('package.json updated successfully!');
