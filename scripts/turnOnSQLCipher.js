febe7e2541d6322487d1ae1cd0f7dce880af5e44
Oscar Franco <ospfranco@gmail.com>
2024-04-22 19:07:26 +0200

    Update SQLCipher

---
+++ scripts/turnOnSQLCipher.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+// Read the package.json file
+const packageJson = JSON.parse(fs.readFileSync('package.json'));
+
+// Modify the op-sqlite.sqlcipher key to true
+packageJson['op-sqlite']['sqlcipher'] = true;
+
+// Save the updated package.json file
+fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+
+console.log('package.json updated successfully!');
