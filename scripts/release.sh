04c8a8fd919a9394a0e22a998bb0c33dcf3292e7
ospfranco <ospfranco@gmail.com>
2024-07-15 07:13:47 -0500

    Release v6.2.10

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ npm --no-git-tag-version version patch
 VERSION=$(node -p "require('./package.json').version")
 echo "Releasing $VERSION"
 git add .
-git commit -m "Release v$(VERSION)"
+git commit -m "Release v$VERSION"
 # git push 
 # git tag "$(VERSION)"
 # git push --tags 
