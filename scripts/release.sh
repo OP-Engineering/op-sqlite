6f33adbd30d42ee6e8231935dd762f8ab05b4b4f
ospfranco <ospfranco@gmail.com>
2024-07-15 07:10:29 -0500

    Release v$(node -p \"require(package.json).version\")

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,10 @@
 set -ex
 
+cd ..
 npm --no-git-tag-version version patch
-git add ..
-git commit -m 'Release v$(node -p \"require('../package.json').version\")' 
+git add .
+git commit -m 'Release v$(node -p \"require('package.json').version\")' 
 git push 
-git tag v$(node -p \"require('../package.json').version\") 
+git tag v$(node -p \"require('package.json').version\") 
 git push --tags 
 npm publish
