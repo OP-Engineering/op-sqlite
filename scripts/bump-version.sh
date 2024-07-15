be337eaf0dc38279c85506d2578448f8c12a660c
ospfranco <ospfranco@gmail.com>
2024-07-15 07:10:02 -0500

    Release v$(node -p \"require(../package.json).version\")

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -8,7 +8,7 @@ git add .
 
 git commit -m "Bump version"
 
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+PACKAGE_VERSION=$(cat ../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
 git tag $PACKAGE_VERSION
 
