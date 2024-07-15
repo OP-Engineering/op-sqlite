f431b7fbe09c796d5f78df7da4be2f4c66e6f00a
ospfranco <ospfranco@gmail.com>
2024-07-15 07:08:23 -0500

    Bump version

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,6 @@ set -ex
 git add ..
 git commit -m 'Release v$(node -p \"require('./package.json').version\")' 
 git push 
-git tag v$(node -p \"require('./package.json').version\") 
+git tag v$(node -p \"require('../package.json').version\") 
 git push --tags 
 npm publish
