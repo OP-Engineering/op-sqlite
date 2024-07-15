1ea66764eeb032f2032f23880820e82b1880d23e
ospfranco <ospfranco@gmail.com>
2024-07-15 07:07:59 -0500

    Bump version

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 set -ex
 
-./scripts/bump-version.sh
-git add .
+./bump-version.sh
+git add ..
 git commit -m 'Release v$(node -p \"require('./package.json').version\")' 
 git push 
 git tag v$(node -p \"require('./package.json').version\") 
