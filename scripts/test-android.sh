34cecc39e84c898c2a8be34c878b65ba76e3f2a7
Oscar Franco <ospfranco@gmail.com>
2024-09-08 09:52:26 -0400

    Update actions

--- scripts/test-android.sh
+++ scripts/test-android.sh
@@ -1,3 +1,6 @@
 JAVA_OPTS=-XX:MaxHeapSize=6g yarn turbo run run:android:release --cache-dir=.turbo/android
 adb reverse tcp:10424 tcp:10424
+echo "🟦 Android device address:"
+adb shell ip addr show
+echo "Polling in-app server..."
 node ./scripts/poll-in-app-server.js
