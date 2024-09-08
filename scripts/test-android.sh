5a7e4a36a2b0eb1052861945a3bf1c5af1fa981b
Oscar Franco <ospfranco@gmail.com>
2024-09-08 08:28:13 -0400

    Update CI

--- scripts/test-android.sh
+++ scripts/test-android.sh
@@ -1,2 +1,2 @@
-JAVA_OPTS=-XX:MaxHeapSize=6g yarn turbo run android --cache-dir=.turbo/android
+JAVA_OPTS=-XX:MaxHeapSize=6g yarn turbo run run:android:release --cache-dir=.turbo/android
 node ./scripts/poll-in-app-server.js
