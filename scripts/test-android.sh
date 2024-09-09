e43f5240673b659a3800b97bac57617a62043e91
Oscar Franco <ospfranco@gmail.com>
2024-09-09 10:01:06 -0400

    Run iOS tests in simulator

--- scripts/test-android.sh
+++ scripts/test-android.sh
@@ -1,8 +1,4 @@
 JAVA_OPTS=-XX:MaxHeapSize=6g yarn turbo run run:android:release --cache-dir=.turbo/android
 adb forward tcp:9000 tcp:9000
-# echo "🟦 Android device address:"
-adb shell ip addr show
-# echo "📱 Android Emulator IP Address:"
-# adb shell ifconfig | grep "inet addr" | awk '{print $2}' | awk -F: '{print $2}'
 echo "Polling in-app server..."
 node ./scripts/poll-in-app-server.js
---
+++ scripts/test-ios.sh
@@ -0,0 +1,4 @@
+yarn turbo run run:ios --cache-dir=.turbo/ios
+
+echo "Polling in-app server..."
+node ./scripts/poll-in-app-server.js
