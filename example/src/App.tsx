197f46e6ee4155716421d5234c17dc1c4a85ce78
Oscar Franco <ospfranco@gmail.com>
2024-09-23 20:52:33 -0400

    Add shareable button

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,13 @@
 import clsx from 'clsx';
 import {useEffect, useState} from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {
+  Button,
+  Clipboard,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import 'reflect-metadata';
 import {constantsTests} from './tests/constants.spec';
 import {registerHooksTests} from './tests/hooks.spec';
@@ -8,6 +15,8 @@ import {blobTests, dbSetupTests, queriesTests, runTests} from './tests/index';
 import {preparedStatementsTests} from './tests/preparedStatements.spec';
 import {reactiveTests} from './tests/reactive.spec';
 import {setServerResults, startServer, stopServer} from './server';
+import {open} from '@op-engineering/op-sqlite';
+import Share from 'react-native-share';
 
 export default function App() {
   const [results, setResults] = useState<any>([]);
@@ -45,9 +54,43 @@ export default function App() {
     return acc && r.type !== 'incorrect';
   }, true);
 
+  const shareDb = async () => {
+    try {
+      const db = open({
+        name: 'shareableDb.sqlite',
+      });
+
+      await db.execute(
+        'CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)',
+      );
+      await db.execute("INSERT INTO test (name) VALUES ('test')");
+      const res = await db.execute('SELECT * FROM test');
+      console.log(res);
+
+      await db.close();
+      await Share.open({
+        url: 'file://' + db.getDbPath(),
+        failOnCancel: false,
+        type: 'application/x-sqlite3',
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const copyDbPathToClipboad = async () => {
+    const db = await open({name: 'shareableDb.sqlite'});
+    const path = db.getDbPath();
+    await db.close();
+    console.warn(path);
+    Clipboard.setString(path);
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-neutral-900">
       <ScrollView>
+        <Button title="Share DB" onPress={shareDb} />
+        <Button title="Copy DB Path" onPress={copyDbPathToClipboad} />
         <Text
           className={clsx('font-bold flex-1 text-white p-2 mt-4', {
             'bg-green-500': allTestsPassed,
