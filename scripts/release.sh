73ff14ec3bc836a5729d51f6e6a24eb74519e4d7
Oscar Franco <ospfranco@gmail.com>
2024-10-07 15:51:22 +0200

    Improve release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,9 @@ set -ex
 
 npm --no-git-tag-version version $1
 VERSION=$(node -p "require('./package.json').version")
-echo "Releasing $VERSION"
+
 git add .
 git commit -m "Release $VERSION"
 git push 
 git tag "$VERSION"
-git push --tags 
+git push --tags
