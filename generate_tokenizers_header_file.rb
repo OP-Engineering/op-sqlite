66c350a25d037fbb5040b5c8e391f2945f000f6c
Oscar Franco <ospfranco@gmail.com>
2024-11-04 12:21:08 +0100

    Partial Android implementation

--- generate_tokenizers_header_file.rb
+++ generate_tokenizers_header_file.rb
@@ -4,10 +4,13 @@ def generate_tokenizers_header_file(names, file_path, is_user_app)
   # Ensure the directory exists
   dir_path = File.dirname(file_path)
   FileUtils.mkdir_p(dir_path) unless Dir.exist?(dir_path)
+  tokenizer_list = names.map { |name| "opsqlite_#{name}_init(db,&errMsg,nullptr);" }.join
 
   File.open(file_path, 'w') do |file|
     file.puts "#ifndef TOKENIZERS_H"
     file.puts "#define TOKENIZERS_H"
+    file.puts
+    file.puts "#define TOKENIZER_LIST #{tokenizer_list}"
     file.puts
     if is_user_app
       file.puts "#include \"node-modules/@op-engineering/op-sqlite/cpp/sqlite3ext.h\""
