e64213af44b58efda434ac111d2155f03a7357c7
Oscar Franco <ospfranco@gmail.com>
2024-11-18 16:04:42 +0700

    Modify Android test script

--- scripts/test-android.sh
+++ scripts/test-android.sh
@@ -1,4 +1,4 @@
-JAVA_OPTS=-XX:MaxHeapSize=6g yarn turbo run run:android:release --cache-dir=.turbo/android
+JAVA_OPTS=-XX:MaxHeapSize=6g yarn android
 adb forward tcp:9000 tcp:9000
 echo "Polling in-app server..."
 node ./scripts/poll-in-app-server.js
