b271b15e4bca02d161d4f7c0318a72edc9bcc72e
Oscar Franco <ospfranco@gmail.com>
2024-11-18 18:03:20 +0700

    CI

--- scripts/test-android.sh
+++ scripts/test-android.sh
@@ -1,5 +1,5 @@
 cd example
-JAVA_OPTS=-XX:MaxHeapSize=6g yarn run:android:release --cache-dir=.turbo/android
+JAVA_OPTS=-XX:MaxHeapSize=6g yarn run:android:release 
 adb forward tcp:9000 tcp:9000
 echo "Polling in-app server..."
 node ../scripts/poll-in-app-server.js
