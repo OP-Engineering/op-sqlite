399bb6c2dcd0f9b1244b68b0dc02af25de619863
Oscar Franco <ospfranco@gmail.com>
2024-12-31 18:56:08 +0100

    Clean ts files

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,14 @@ declare global {
 
 if (global.__OPSQLiteProxy == null) {
   if (NativeModules.OPSQLite == null) {
-    throw new Error('Base module not found. Maybe try rebuilding the app.');
-  }
-
-  if (NativeModules.OPSQLite.install == null) {
     throw new Error(
-      'Failed to install op-sqlite: React Native is not running on-device. OPSQLite can only be used when synchronous method invocations (JSI) are possible. If you are using a remote debugger (e.g. Chrome), switch to an on-device debugger (e.g. Flipper) instead.'
+      'Base module not found. Did you do a pod install/clear the gradle cache?'
     );
   }
 
   // Call the synchronous blocking install() function
-  const result = NativeModules.OPSQLite.install();
-  if (result !== true) {
+  const installed = NativeModules.OPSQLite.install();
+  if (!installed) {
     throw new Error(
       `Failed to install op-sqlite: The native OPSQLite Module could not be installed! Looks like something went wrong when installing JSI bindings, check the native logs for more info`
     );
@@ -27,7 +23,7 @@ if (global.__OPSQLiteProxy == null) {
   // Check again if the constructor now exists. If not, throw an error.
   if (global.__OPSQLiteProxy == null) {
     throw new Error(
-      'Failed to install op-sqlite, the native initializer function does not exist. Are you trying to use OPSQLite from different JS Runtimes?'
+      'OPSqlite native object is not available. Something is wrong. Check the native logs for more information.'
     );
   }
 }
@@ -44,171 +40,6 @@ export const {
 } = !!NativeModules.OPSQLite.getConstants
   ? NativeModules.OPSQLite.getConstants()
   : NativeModules.OPSQLite;
-
-type Scalar = string | number | boolean | null | ArrayBuffer | ArrayBufferView;
-
-/**
- * Object returned by SQL Query executions {
- *  insertId: Represent the auto-generated row id if applicable
- *  rowsAffected: Number of affected rows if result of a update query
- *  message: if status === 1, here you will find error description
- *  rows: if status is undefined or 0 this object will contain the query results
- * }
- *
- * @interface QueryResult
- */
-export type QueryResult = {
-  insertId?: number;
-  rowsAffected: number;
-  res?: any[];
-  rows: Array<Record<string, Scalar>>;
-  // An array of intermediate results, just values without column names
-  rawRows?: Scalar[][];
-  columnNames?: string[];
-  /**
-   * Query metadata, available only for select query results
-   */
-  metadata?: ColumnMetadata[];
-};
-
-/**
- * Column metadata
- * Describes some information about columns fetched by the query
- */
-export type ColumnMetadata = {
-  /** The name used for this column for this result set */
-  name: string;
-  /** The declared column type for this column, when fetched directly from a table or a View resulting from a table column. "UNKNOWN" for dynamic values, like function returned ones. */
-  type: string;
-  /**
-   * The index for this column for this result set*/
-  index: number;
-};
-
-/**
- * Allows the execution of bulk of sql commands
- * inside a transaction
- * If a single query must be executed many times with different arguments, its preferred
- * to declare it a single time, and use an array of array parameters.
- */
-export type SQLBatchTuple = [string] | [string, Array<any> | Array<Array<any>>];
-
-export type UpdateHookOperation = 'INSERT' | 'DELETE' | 'UPDATE';
-
-/**
- * status: 0 or undefined for correct execution, 1 for error
- * message: if status === 1, here you will find error description
- * rowsAffected: Number of affected rows if status == 0
- */
-export type BatchQueryResult = {
-  rowsAffected?: number;
-};
-
-/**
- * Result of loading a file and executing every line as a SQL command
- * Similar to BatchQueryResult
- */
-export interface FileLoadResult extends BatchQueryResult {
-  commands?: number;
-}
-
-export interface Transaction {
-  commit: () => Promise<QueryResult>;
-  execute: (query: string, params?: Scalar[]) => Promise<QueryResult>;
-  rollback: () => Promise<QueryResult>;
-}
-
-export interface PendingTransaction {
-  /*
-   * The start function should not throw or return a promise because the
-   * queue just calls it and does not monitor for failures or completions.
-   *
-   * It should catch any errors and call the resolve or reject of the wrapping
-   * promise when complete.
-   *
-   * It should also automatically commit or rollback the transaction if needed
-   */
-  start: () => void;
-}
-
-export type PreparedStatementObj = {
-  bind: (params: any[]) => Promise<void>;
-  execute: () => Promise<QueryResult>;
-};
-
-export type DB = {
-  close: () => void;
-  delete: (location?: string) => void;
-  attach: (
-    mainDbName: string,
-    dbNameToAttach: string,
-    alias: string,
-    location?: string
-  ) => void;
-  detach: (mainDbName: string, alias: string) => void;
-  transaction: (fn: (tx: Transaction) => Promise<void>) => Promise<void>;
-  executeSync: (query: string, params?: Scalar[]) => QueryResult;
-  execute: (query: string, params?: Scalar[]) => Promise<QueryResult>;
-  executeWithHostObjects: (
-    query: string,
-    params?: Scalar[]
-  ) => Promise<QueryResult>;
-  executeBatch: (commands: SQLBatchTuple[]) => Promise<BatchQueryResult>;
-  loadFile: (location: string) => Promise<FileLoadResult>;
-  updateHook: (
-    callback?:
-      | ((params: {
-          table: string;
-          operation: UpdateHookOperation;
-          row?: any;
-          rowId: number;
-        }) => void)
-      | null
-  ) => void;
-  commitHook: (callback?: (() => void) | null) => void;
-  rollbackHook: (callback?: (() => void) | null) => void;
-  prepareStatement: (query: string) => PreparedStatementObj;
-  loadExtension: (path: string, entryPoint?: string) => void;
-  executeRaw: (query: string, params?: Scalar[]) => Promise<any[]>;
-  getDbPath: (location?: string) => string;
-  reactiveExecute: (params: {
-    query: string;
-    arguments: any[];
-    fireOn: {
-      table: string;
-      ids?: number[];
-    }[];
-    callback: (response: any) => void;
-  }) => () => void;
-  /** This function is only available for libsql.
-   * Allows to trigger a sync the database with it's remote replica
-   * In order for this function to work you need to use openSync or openRemote functions
-   * with libsql: true in the package.json
-   *
-   * The database is hosted in turso
-   **/
-  sync: () => void;
-  flushPendingReactiveQueries: () => Promise<void>;
-};
-
-type OPSQLiteProxy = {
-  open: (options: {
-    name: string;
-    location?: string;
-    encryptionKey?: string;
-  }) => DB;
-  openRemote: (options: { url: string; authToken: string }) => DB;
-  openSync: (options: {
-    url: string;
-    authToken: string;
-    name: string;
-    location?: string;
-    syncInterval?: number;
-  }) => DB;
-  isSQLCipher: () => boolean;
-  isLibsql: () => boolean;
-  isIOSEmbedded: () => boolean;
-};
 
 const locks: Record<
   string,
