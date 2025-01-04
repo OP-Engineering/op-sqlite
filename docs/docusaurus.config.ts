a1ba6b04dc435eb25a27f37e23422fa5b14aef8d
Oscar Franco <ospfranco@gmail.com>
2025-01-04 11:20:43 +0100

    Fix base url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://op-engineering.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/op-sqlite',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
