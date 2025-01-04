a0bcbf5723f1ee6c366855764f8a16f8a663b147
Oscar Franco <ospfranco@gmail.com>
2025-01-04 11:28:35 +0100

    Modify header

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -78,7 +78,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {
           href: 'https://github.com/OP-Engineering/op-sqite',
