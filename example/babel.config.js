51db6f665601cec821725ba6be4f84d9210b3a57
Oscar Franco <ospfranco@gmail.com>
2025-08-24 21:33:09 +0200

    Fix babel plugins

--- example/babel.config.js
+++ example/babel.config.js
@@ -7,19 +7,7 @@ const root = path.resolve(__dirname, '..');
 module.exports = getConfig(
   {
     presets: ['module:@react-native/babel-preset'],
-    plugins: [
-      'nativewind/babel',
-      [
-        'module-resolver',
-        {
-          alias: {
-            stream: 'stream-browserify',
-          },
-        },
-      ],
-      'babel-plugin-transform-typescript-metadata',
-      ['@babel/plugin-proposal-decorators', {legacy: true}],
-    ],
+    plugins: ['nativewind/babel'],
   },
   {root, pkg},
 );
