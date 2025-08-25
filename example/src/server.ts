b3d5c2f911a0f150577f5371295c32aedfc817c6
Oscar Franco <ospfranco@gmail.com>
2025-08-25 22:18:36 +0200

    Fix server?

--- example/src/server.ts
+++ example/src/server.ts
@@ -1,26 +1,25 @@
 import {BridgeServer} from 'react-native-http-bridge-refurbished';
+
 let passed: boolean | null = null;
 
 const server = new BridgeServer('http_service', true);
 
-server.get('/ping', async (_req, _res) => {
-  return {message: 'pong'};
+server.get('/ping', async (_req, res) => {
+  res.send(200, 'application/text', 'pong');
 });
 
-server.get('/results', async (_req, _res) => {
-  return {passed};
+server.get('/results', async (_req, res) => {
+  res.json({passed}, 200);
 });
 
 server.listen(9000);
-
-export function startServer() {
-  return server;
-}
+console.log('Server listening on port 9000');
 
 export function stopServer() {
   server.stop();
 }
 
 export function setServerResults(r: boolean) {
+  console.log('Setting server results to', r);
   passed = r;
 }
