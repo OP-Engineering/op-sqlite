44e25aaccb0782d1f9bdbc73e838130197940f7e
Oscar Franco <ospfranco@gmail.com>
2025-12-23 11:18:14 -0400

    Print device logs

--- scripts/test-ios.sh
+++ scripts/test-ios.sh
@@ -11,3 +11,16 @@ sleep 5
 cd ..
 
 node ./scripts/poll-in-app-server.js
+
+if [ $? -ne 0 ]; then
+  echo "poll-in-app-server failed, printing device logs..."
+  DEVICE_ID=$(xcrun simctl list devices booted | grep -m1 Booted | awk -F '[()]' '{print $2}')
+  if [ -n "$DEVICE_ID" ]; then
+    xcrun simctl spawn "$DEVICE_ID" log stream --style syslog --predicate 'eventType = logEvent' --last 1h
+  else
+    echo "No booted simulator device found."
+  fi
+fi
+```
+
+
