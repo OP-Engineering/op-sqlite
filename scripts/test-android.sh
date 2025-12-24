ba536a2f2d1f117c96f98ef3d0256f2331a9c671
Oscar Franco <ospfranco@gmail.com>
2025-12-24 14:40:56 -0400

    Sleep a minute

--- scripts/test-android.sh
+++ scripts/test-android.sh
@@ -15,7 +15,7 @@ adb forward tcp:9000 tcp:9000
 yarn run:android:release
 
 echo "Waiting 20 seconds for app to fully initialize and tests to start..."
-sleep 40
+sleep 60
 
 echo "Checking if app is running..."
 adb shell "ps | grep com.op.sqlite.example" || {
