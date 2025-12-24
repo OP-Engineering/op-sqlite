133c9d0b6f20535ef287fdd8e92fca081fdecc93
Oscar Franco <ospfranco@gmail.com>
2025-12-24 09:40:27 -0400

    Wait for device to fully boot

--- scripts/test-android.sh
+++ scripts/test-android.sh
@@ -4,10 +4,16 @@ set -ex
 cd example || exit
 
 adb wait-for-device
+echo "Waiting for boot to complete..."
+adb shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done'
+echo "Boot completed!"
 adb shell input keyevent 82
 adb forward tcp:9000 tcp:9000
 
-JAVA_OPTS=-XX:MaxHeapSize=6g yarn run:android:release 
+JAVA_OPTS=-XX:MaxHeapSize=6g yarn run:android:release
+
+echo "Waiting 10 seconds for app to start..."
+sleep 10 
 
 node ../scripts/poll-in-app-server.js || {
   echo "❌ poll-in-app-server failed, printing device logs..."
